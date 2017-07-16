a9f5d6358f3cfcd0bd9b9cb85ddd6efed48cc4e4
voonlong <voonlong@gmail.com>
2017-07-16 22:20:39 +0800

    assign user role, partial complete, workable

--- src/main/java/com/chlorocode/tendertracker/web/admin/AdminController.java
+++ src/main/java/com/chlorocode/tendertracker/web/admin/AdminController.java
@@ -1,10 +1,23 @@
 package com.chlorocode.tendertracker.web.admin;
 
+import com.chlorocode.tendertracker.dao.entity.User;
+import com.chlorocode.tendertracker.exception.ResourceNotFoundException;
+import com.chlorocode.tendertracker.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class AdminController {
+
+    private UserService userService;
+
+    @Autowired
+    public AdminController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/admin")
     public String showDashboardPage() {
@@ -14,5 +27,18 @@ public class AdminController {
     @GetMapping("/admin/userDetails")
     public String showUserDetails() {
         return "admin/user/userView";
+    }
+
+    @GetMapping("/admin/userDetails/{id}")
+    public String displayUserDetail(@PathVariable(value="id") Integer id, Model model) {
+        User user = userService.findById(id);
+        if (user == null) {
+            throw new ResourceNotFoundException();
+        } else {
+            model.addAttribute("reg", user);
+
+            return "admin/user/assignUerRole";
+        }
+
     }
 }
