30b0cef8663146eb00e093003bbb30c33bfab6e5
Kevin <nkevinsst@gmail.com>
2017-12-04 21:06:09 +0800

    Add security for Web API

--- src/main/java/com/chlorocode/tendertracker/api/WebCrawlerController.java
+++ src/main/java/com/chlorocode/tendertracker/api/WebCrawlerController.java
@@ -3,9 +3,15 @@ package com.chlorocode.tendertracker.api;
 import com.chlorocode.tendertracker.dao.entity.ExternalTender;
 import com.chlorocode.tendertracker.logging.TTLogger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.Serializable;
+import java.nio.charset.Charset;
+import java.util.Base64;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -19,6 +25,12 @@ public class WebCrawlerController {
     @Autowired
     private LongProcess longProcess;
 
+    @Value("${crawler.username}")
+    private String crawlerUsername;
+
+    @Value("${crawler.password}")
+    private String crawlerPassword;
+
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
@@ -29,10 +41,36 @@ public class WebCrawlerController {
     }
 
     @RequestMapping(path = "/tenders", method = RequestMethod.POST)
-    public String tenders(@RequestBody List<ExternalTender> tenders) {
+    public ResponseEntity tenders(@RequestBody List<ExternalTender> tenders, HttpServletRequest request) {
         TTLogger.debug(this.getClass().getName(),"*******************WebCrawlerController" + this.toString() + "*********************");
-        longProcess.createExternalTenderList(tenders);
-        return "success";
+
+        // Validate authentication
+        final String authorization = request.getHeader("Authorization");
+        if (authorization != null && authorization.startsWith("Basic")) {
+            String base64Credentials = authorization.substring("Basic".length()).trim();
+            String credentials = new String(Base64.getDecoder().decode(base64Credentials),
+                    Charset.forName("UTF-8"));
+            // credentials = username:password
+            final String[] values = credentials.split(":", 2);
+
+            if (values.length != 2 || !values[0].equals(crawlerUsername) || !values[1].equals(crawlerPassword)) {
+                return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+            }
+        } else {
+            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+        }
+
+        // Proccess the request
+        try {
+            longProcess.createExternalTenderList(tenders);
+        } catch (Exception ex) {
+            TTLogger.error(this.getClass().getName(), "Error when saving External Tender", ex);
+            ResponseEntity responseEntity = new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+            return responseEntity;
+        }
+
+        ResponseEntity responseEntity = new ResponseEntity(HttpStatus.OK);
+        return responseEntity;
     }
 
     public class Greeting implements Serializable {
