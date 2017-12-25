0cc5ea6b76fa417e53c89dee04cdd146db379e14
Raghavendra Shanthappa <ss.raghavendra@gmail.com>
2017-12-25 12:17:51 +0800

    reports changes

--- src/main/java/com/chlorocode/tendertracker/web/reports/TenderReportsController.java
+++ src/main/java/com/chlorocode/tendertracker/web/reports/TenderReportsController.java
@@ -1,5 +1,6 @@
 package com.chlorocode.tendertracker.web.reports;
 
+import com.chlorocode.tendertracker.dao.dto.AlertDTO;
 import com.chlorocode.tendertracker.dao.dto.ProcurementReportDTO;
 import com.chlorocode.tendertracker.service.notification.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -33,18 +35,34 @@ public class TenderReportsController {
         return "admin/reports/reportsDashboard";
     }
 
-    @PostMapping("/admin/tender/reportdetails")
-    public String showReportDetails(@RequestParam("reportType") String reportType, ModelMap model) throws ParseException {
+    @PostMapping("/admin/tender/reports")
+    public String showReportDetails(@RequestParam("reportType") String reportType, ModelMap model,
+                                    HttpServletRequest request) {
 
         System.out.println("reportType: " + reportType);
+        String startDate = request.getParameter("startDate");
+        String endDate = request.getParameter("endDate");
+        //System.out.println("startDate: " + request.getParameter("startDate"));
+        //System.out.println("endDate: " + request.getParameter("endDate"));
 
-        DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
-        List<ProcurementReportDTO> procurementReportList = reportService.findAllByDateRange(df.parse("01-06-2017"), df.parse("31-12-2017"));
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+        List<ProcurementReportDTO> procurementReportList = null;
+        try {
+            procurementReportList = reportService.findAllByDateRange(df.parse(startDate), df.parse(endDate));
+            for (ProcurementReportDTO procurementReportDTO : procurementReportList) {
+                System.out.println(procurementReportDTO.getRefNum() + " : " + procurementReportDTO.getTenderName() );
+            }
+            model.addAttribute("selectedReportType", reportType);
+
+        } catch (ParseException ex) {
+            AlertDTO alert = new AlertDTO(AlertDTO.AlertType.DANGER,
+                    ex.getMessage());
+            model.addAttribute("alert", alert);
+        }
 
         //for (int i=0; i<procurementReportList.size();i++)
-            System.out.println(procurementReportList);
+            //System.out.println(procurementReportList);
 
-        model.addAttribute("selectedReportType", reportType);
         return "admin/reports/reportsDashboard";
     }
 }
